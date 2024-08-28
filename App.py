527c20d9529893abdbd91fb5aa8ff6b7cb68c3c7
Yesjary <allancbtis199@gmail.com>
2024-08-28 11:28:33 -0600

    Modificacion del achivo App

--- App.py
+++ App.py
@@ -14,10 +14,39 @@ db = client['face_database']
 fs = gridfs.GridFS(db)
 people_collection = db['people']
 
-@app.route('/add_and_validate_person', methods=['POST'])
-def add_and_validate_person():
+@app.route('/add_person', methods=['POST'])
+def add_person():
     try:
         name = request.form['name']
+        image_file = request.files['image']
+        image = Image.open(image_file)
+        image = np.array(image)
+
+        encodings = face_recognition.face_encodings(image)
+        
+        if not encodings:
+            return jsonify({"error": "No se encontraron codificaciones faciales en la imagen."})
+        
+        encoding = encodings[0].tolist()
+
+        # Guardar la imagen en GridFS
+        image_id = fs.put(image_file, filename=name)
+
+        # Guardar los datos en la colección
+        people_collection.insert_one({
+            'name': name,
+            'image_id': image_id,
+            'encoding': encoding
+        })
+
+        return jsonify({"message": "Persona anadida con exito."})
+
+    except Exception as e:
+        return jsonify({"error": f'Ocurrio un error: {e}'})
+
+@app.route('/validate_person', methods=['POST'])
+def validate_person():
+    try:
         image_file = request.files['image']
         image = Image.open(image_file)
         image = np.array(image)
@@ -45,9 +74,7 @@ def add_and_validate_person():
         })
 
     except Exception as e:
-        return jsonify({"error": f'Ocurrió un error: {e}'})
-
-
+        return jsonify({"error": f'Ocurrio un error: {e}'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
